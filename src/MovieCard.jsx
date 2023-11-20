9f4fbd0bff0adb07ae5b7cc8fb004d2bddeaf5e8
sami <samiap914@gmail.com>
2023-11-20 18:52:15 +0530

    test

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -9,7 +9,7 @@ const MovieCard = ({movie}) => {
         <div>
             <img src={movie.Poster !== 'N/A' ?movie.Poster: 'https://via.placeholder.com/400'} alt={movie.Title}/>
         </div>
-        <div>
+        <div> 
             <span>{movie.Type}</span>
             <h3>{movie.Title}</h3>
         </div>
